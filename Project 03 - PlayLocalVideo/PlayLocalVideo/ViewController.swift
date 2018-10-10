96d6c65a7a0efc66f3157c24e1410f40d494c61e
Tu Zhuoqi <35286309+kevintzq@users.noreply.github.com>
2018-10-10 17:03:54 +0800

    3.本地视频播放

---
+++ Project 03 - PlayLocalVideo/PlayLocalVideo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlayLocalVideo
+//
+//  Created by 卓奇 屠 on 2018/10/10.
+//  Copyright © 2018 卓奇 屠. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
