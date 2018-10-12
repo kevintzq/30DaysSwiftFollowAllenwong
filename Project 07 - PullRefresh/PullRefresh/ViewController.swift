5ceb4927f0988bfb538f596e213001d6fe9abf12
kevintzq <514104840@qq.com>
2018-10-12 22:58:38 +0800

    7.下拉刷新

--- Project 07 - PullRefresh/PullRefresh/ViewController.swift
+++ Project 07 - PullRefresh/PullRefresh/ViewController.swift
@@ -8,13 +8,68 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    let cellIdentifier = "NewCellIdentifier"
+    let emoji = ["🤗🤗🤗🤗🤗", "😅😅😅😅😅", "😆😆😆😆😆"]
+    let newEmoji = ["🏃🏃🏃🏃🏃", "💩💩💩💩💩", "👸👸👸👸👸",
+                    "🤗🤗🤗🤗🤗", "😅😅😅😅😅", "😆😆😆😆😆" ]
+    var emojiData = [String]()
+    var tableViewController = UITableViewController(style: .plain)
+    var navBar: UINavigationBar = UINavigationBar(frame: CGRect(x: 0, y: 0, width: 375, height: 64))
+    var refreshControl = UIRefreshControl()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        emojiData = emoji
+        let emojiTableView = tableViewController.tableView
+        emojiTableView?.backgroundColor = UIColor.darkGray
+        emojiTableView?.dataSource = self
+        emojiTableView?.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+        emojiTableView?.rowHeight = UITableView.automaticDimension
+        emojiTableView?.estimatedRowHeight = 60.0
+        emojiTableView?.tableFooterView = UIView(frame: CGRect.zero)
+        emojiTableView?.separatorStyle = UITableViewCell.SeparatorStyle.none
+        
+        tableViewController.refreshControl = self.refreshControl
+        self.refreshControl.addTarget(self, action: #selector(ViewController.didRoadEmoji), for: .valueChanged)
+        self.refreshControl.backgroundColor = UIColor.lightGray
+        let attributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        self.refreshControl.attributedTitle = NSAttributedString(string: "Last update on \(Date())", attributes: attributes)
+        self.refreshControl.tintColor = UIColor.white
+        self.title = "emoji"
+        self.navBar.barStyle = UIBarStyle.blackTranslucent
+        
+        self.view.addSubview(emojiTableView!)
+        self.view.addSubview(navBar)
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojiData.count
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)! as UITableViewCell
+        
+        cell.textLabel!.text = self.emojiData[indexPath.row]
+        cell.textLabel!.textAlignment = NSTextAlignment.center
+        cell.textLabel!.font = UIFont.systemFont(ofSize: 50)
+        cell.backgroundColor = UIColor.clear
+        cell.selectionStyle = UITableViewCell.SelectionStyle.none
+        
+        return cell
+    }
+    
+    @objc func didRoadEmoji() {
+        self.emojiData = newEmoji
+        self.tableViewController.tableView.reloadData()
+        self.refreshControl.endRefreshing()
+    }
 
 }
 
