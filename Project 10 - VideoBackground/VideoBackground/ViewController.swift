034c91c98d5721207ce294d075762bfe315ead56
kevintzq <514104840@qq.com>
2018-10-14 19:28:34 +0800

    10.视频背景（未完成）

---
+++ Project 10 - VideoBackground/VideoBackground/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoBackground
+//
+//  Created by Zhuoqi Tu on 2018/10/14.
+//  Copyright © 2018 Zhuoqi Tu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+
+}
+
