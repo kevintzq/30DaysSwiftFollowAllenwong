a06f5924fd507950a4f1b14117c1f154eafe3c1f
kevintzq <514104840@qq.com>
2018-10-14 21:00:34 +0800

    11.渐变TableView

---
+++ Project 11 - ClearPrioritizeTableViewCell/ClearPrioritizeTableViewCell/TableViewCell.swift
@@ -0,0 +1,49 @@
+//
+//  TableViewCell.swift
+//  ClearPrioritizeTableViewCell
+//
+//  Created by Zhuoqi Tu on 2018/10/14.
+//  Copyright © 2018 Zhuoqi Tu. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    let gradientLayer = CAGradientLayer()
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        let color1 = UIColor(white: 1.0, alpha: 0.2).cgColor
+        let color2 = UIColor(white: 1.0, alpha: 0.1).cgColor
+        let color3 = UIColor.clear.cgColor
+        let color4 = UIColor(white: 0.0, alpha: 0.05).cgColor
+        
+        gradientLayer.frame = self.bounds
+        gradientLayer.colors = [color1, color2, color3, color4]
+        gradientLayer.locations = [0.0, 0.04, 0.95, 1.0]
+        layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        gradientLayer.frame = self.bounds
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
